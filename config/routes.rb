d826d8a4fd9848ed997e99bcb06e961898eea8b0
Henrique Souza dos Santos <h.s.s_henrique@hotmail.com>
2023-04-13 20:22:09 -0300

    URL Helpers lesson

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@
 
 Rails.application.routes.draw do
   # GET /about
-  get 'about', to: 'about#index'
+  # get 'about', to: 'about#index'
+
+  # modify the path /about to /about-us
+  get 'about-us', to: 'about#index', as: :about
 
   # get '/', to: 'main#index'
 

c64f6d8c5bc8ad409c7b694e0e9e68187d86c583
Henrique Souza dos Santos <h.s.s_henrique@hotmail.com>
2023-04-25 08:00:32 -0300

    update user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@
 # password_confirmation:string virtual
 class User < ApplicationRecord
   has_secure_password
+
+  validates :email, presence: true, format: { with: /\A[\w.+-]+@\w+\.\w+\z/, on: :create }
 end

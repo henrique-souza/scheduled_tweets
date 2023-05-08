6bfa8b1b4e46c10d0548bbe77fa2ac368c5d8612
Henrique Souza dos Santos <h.s.s_henrique@hotmail.com>
2023-05-08 08:00:59 -0300

    update password regex and cookies

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,7 +2,11 @@
 
 # Main controller
 class MainController < ApplicationController
-  def index; end
+  def index
+    return unless session[:user_id]
+
+    @user = User.find_by(id: session[:user_id])
+  end
   # Exemplo de uso do flash
   #   flash.now[:notice] = 'Logged in successfully'
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,21 +8,11 @@
 class User < ApplicationRecord
   has_secure_password
 
-  PASSWORD_REGEX = /\A
-                   (?=.{8,})
-                   (?=.*\d)
-                   (?=.*[a-z])F
-                   (?=.*[A-Z])
-                   (?=.*[[:^alnum:]])
-                   /x
-
   validates :email,
             presence: true,
             format: { with: /\A[\w.+-]+@\w+\.\w+\z/, on: :create },
             uniqueness: true
 
   validates :password,
-            length: { in: 8..20 },
-            format: { with: PASSWORD_REGEX },
-            allow_blank: false
+            length: { in: 8..100 }
 end

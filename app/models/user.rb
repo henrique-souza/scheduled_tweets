98c10f870c559113452f6e76bdbf41636b4b7806
Henrique Souza dos Santos <h.s.s_henrique@hotmail.com>
2023-05-07 23:36:44 -0300

    update password regex validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,21 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :email, presence: true, format: { with: /\A[\w.+-]+@\w+\.\w+\z/, on: :create }, uniqueness: true
+  PASSWORD_REGEX = /\A
+                    (?=.{8,})
+                    (?=.*\d)
+                    (?=.*[a-z])F
+                    (?=.*[A-Z])
+                    (?=.*[[:^alnum:]])
+                    /x
+
+  validates :email,
+            presence: true,
+            format: { with: /\A[\w.+-]+@\w+\.\w+\z/, on: :create },
+            uniqueness: true
+
+  validates :password,
+            length: { in: 8..20 },
+            format: { with: PASSWORD_REGEX },
+            allow_blank: false
 end

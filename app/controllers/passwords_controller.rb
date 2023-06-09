ae929795e077d9df7e897154be40dcb01eabd7a1
Henrique Souza dos Santos <h.s.s_henrique@hotmail.com>
2023-06-09 14:49:41 -0300

    adding password edit to routes and user logged

---
+++ app/controllers/passwords_controller.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# Controller to password methods
+class PasswordsController < ApplicationController
+  before_action :require_user_logged_in!
+
+  def edit; end
+
+  def update; end
+end

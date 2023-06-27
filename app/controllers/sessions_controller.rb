28cb7a058d3356457b988be15f845947d9582377
Henrique Souza dos Santos <h.s.s_henrique@hotmail.com>
2023-06-27 05:24:04 -0300

    remove a bug in session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    user = User.find_by(email: params[:email][:password])
+    user = User.find_by(email: params[:email])
 
     if user.present?
       session[:user_id] = user.id

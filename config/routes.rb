02e2907f2ba51e4af900af6c5012203d9d328f3d
Henrique Souza dos Santos <h.s.s_henrique@hotmail.com>
2023-07-02 14:25:04 -0300

    bug fix: passwords confirmations on login

--- config/routes.rb
+++ config/routes.rb
@@ -21,7 +21,6 @@ Rails.application.routes.draw do
 
   get 'sign_in', to: 'sessions#new'
   post 'sign_in', to: 'sessions#create'
-
   delete 'logout', to: 'sessions#destroy'
 
   get 'password/reset', to: 'password_resets#new'

88d9da3a7fcfbf5ac308262eafb49b375535303c
Henrique Souza dos Santos <h.s.s_henrique@hotmail.com>
2023-07-02 22:16:26 -0300

    forgot password forms and methods

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -4,5 +4,13 @@
 class PasswordResetsController < ApplicationController
   def new; end
 
-  def create; end
+  def create
+    @user = User.find_by(email: params[:email])
+    # Send e-mail
+
+    return unless @user.present?
+
+    redirect_to root_path, notice: 'If an account with that e-mail was found,
+      we have sent a link to reset your password'
+  end
 end

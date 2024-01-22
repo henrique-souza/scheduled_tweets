5212fcf11d413fa2f981cb2e26cf803744a0a9a7
Henrique Souza dos Santos <h.s.s_henrique@hotmail.com>
2024-01-22 13:30:23 -0300

    adding password update with a token

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -7,6 +7,7 @@ class PasswordResetsController < ApplicationController
   def create
     @user = User.find_by(email: params[:email])
 
+    # Send e-mail
     PasswordMailer.with(user: @user).reset.deliver_now if @user.present?
 
     redirect_to root_path, notice: 'If an account with that e-mail was found,
@@ -14,6 +15,24 @@ class PasswordResetsController < ApplicationController
   end
 
   def edit
-    @user = User.find_signed(params[:token], purpose: 'password_reset')
+    @user = User.find_signed!(params[:token], purpose: 'password_reset')
+  rescue ActiveSupport::MessageVerifier::InvalidSignature
+    redirect_to sign_in_path, alert: 'Your token has expired. Please, try again'
+  end
+
+  def update
+    @user = User.find_signed!(params[:token], purpose: 'password_reset')
+
+    if @user.update(password_params)
+      redirect_to sign_in_path, notice: 'Your password was reset successfully. Please, sign in.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def password_params
+    params.require(:user).permit(:password, :password_confirmation)
   end
 end

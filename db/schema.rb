5286f5bff9f84cf7551ca6bf1a3e3aedf89a22df
Henrique Souza dos Santos <h.s.s_henrique@hotmail.com>
2024-04-15 18:00:20 -0300

    update: twitter_accounts and docs

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_18_114028) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_15_205034) do
   create_table "currents", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "twitter_accounts", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "name"
+    t.string "username"
+    t.string "image"
+    t.string "token"
+    t.string "secret"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_twitter_accounts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -23,4 +35,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_18_114028) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "twitter_accounts", "users"
 end
